a693a88162a40c0851e2f62d989d0c65e686c087
Fukui1372 <kennosuke1372@icloud.com>
2024-02-05 21:36:05 +0900

    Usersテーブルの修正

--- database/migrations/2023_12_18_083655_create_users_table.php
+++ database/migrations/2023_12_18_083655_create_users_table.php
@@ -19,8 +19,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->integer('collected_points');
+            $table->integer('collected_points')->default(0);
             $table->rememberToken();
+            $table->timestamps();
         });
     }
 
